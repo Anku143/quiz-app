c2c05fe1473ca66f9e8c9d6de187a7b8345dde34
Ankush <ankush.r5621@gmail.com>
2025-04-03 11:24:01 +0530

    project complete

--- src/components/QuestionIndex.jsx
+++ src/components/QuestionIndex.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const QuestionIndex = ({ question, options, handleClick,currentAnswer, handleNext }) => {
+const QuestionIndex = ({ question, options, handleClick, currentAnswer, handleNext }) => {
     return (
         <div className='flex justify-center items-center flex-col h-screen'>
             <h2 className='text-xl mb-5 font-bold'>{question}</h2>
             <ul>
                 {options.map((option, index) => (
-                    <li key={index} className={`bg-gray-400 p-2 m-3 w-75 text-center cursor-pointer hover:bg-white duration-300 font-medium ${currentAnswer === option ? 'selected' : ''}`} onClick={()=> handleClick(option)}>
+                    <li key={index} className={`bg-gray-400 p-2 m-3 w-75 text-center cursor-pointer hover:bg-white duration-300 font-medium ${currentAnswer === option ? 'selected' : ''}`} onClick={() => handleClick(option)}>
                         {option}
                     </li>
                 ))}
             </ul>
-            <button disabled={currentAnswer === null}  onClick={handleNext}className={`bg-gray-400 hover:bg-white duration-300 w-75 p-2 m-3 rounded-2xl cursor-pointer ${currentAnswer === null ? 'disable' : ''}`}>Next Question</button>
+            <button disabled={currentAnswer === null} onClick={()=>handleNext()} className={`bg-gray-400 hover:bg-white duration-300 w-75 p-2 m-3 rounded-2xl cursor-pointer ${currentAnswer === null ? 'disable' : ''}`}>Next Question</button>
         </div>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,14 +3,31 @@ import QuestionIndex from './QuestionIndex'
 
 const Quiz = () => {
     const question = [{
-        "question": "1. What is the capital of France?",        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "answer": "Paris"
+        question: "1. What is the capital of France?",        
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer: "Paris"
     },
     {
-        "question": "2. What is the capital of Germany?",
-        "options": ["Paris", "Delhi", "Berlin", "Madrid"],
-        "answer": "Berlin"
-    }]
+        question: "2. What is the capital of Germany?",
+        options: ["Paris", "Delhi", "Berlin", "Madrid"],
+        answer: "Berlin"
+    },
+    {
+        question: "3. What is the capital of India?",
+        options: ["Paris", "Delhi", "Berlin", "Madrid"],
+        answer: "Delhi"
+    },
+    {
+        question: "4. What is the capital of Spain?",
+        options: ["Paris", "London", "Berlin", "Madrid"],
+        answer: "Madrid"
+    },
+    {
+        question: "5. What is the capital of Italy?",
+        options: ["Paris", "London", "Berlin", "Rome"],
+        answer: "Rome"
+    }
+]
 
     const [questionsList, setQuestionsList] = useState(0)
     const [currentAnswer, setCurrentAnswer] = useState(null)
@@ -18,28 +35,42 @@ const Quiz = () => {
 
     const handleClick = (option)=>{
         setCurrentAnswer(option)
+        console.log(currentAnswer)
         if(option === question[questionsList].answer ){
-            setScore(score + 1 )
+            setScore(prevScore => prevScore + 1 )
         }
     }
 
-    const handleNext=()=>{
-        setQuestionsList(questionsList + 1)
-        currentAnswer(null)
-
-    }
+    const handleNext = () => {
+        setQuestionsList(prev => prev + 1);
+        setCurrentAnswer(null); 
+    };
+    
 
     return (
         <>
         {questionsList < question.length ? 
         <div>
-            <QuestionIndex question={question[questionsList].question} options={question[questionsList].options} setQuestionsList={setQuestionsList} questionsList={questionsList}  handleClick={handleClick} currentAnswer={currentAnswer} handleNext={handleNext}/>
+            <QuestionIndex 
+        question={question[questionsList].question} 
+        options={question[questionsList].options} 
+        questionsList={questionsList}  
+        handleClick={handleClick} 
+        currentAnswer={currentAnswer} 
+        handleNext={handleNext}/>
                 
 
         </div>
-   : <div>Your Score is {score} out of {question.length}</div> }
+   : <div className='
+   flex justify-center items-center h-screen '>
+   <div className='p-5 bg-amber-50 rounded-xl'>
+   Your Score is {score} out of {question.length}
+   </div>
+   </div> 
+   }
         </>
     )
 }
 
 export default Quiz
+
